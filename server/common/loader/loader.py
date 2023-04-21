2120406a0df0c8bdaecc47d9dd39a35eef8359d4
leogm99 <lgiampier62@gmail.com>
2023-04-21 16:55:18 -0300

    Fix: loader constructor fields where unchanged in the last commit, and the rabbit connection was not setup.

--- server/common/loader/loader.py
+++ server/common/loader/loader.py
@@ -2,7 +2,7 @@ import socket
 import logging
 import signal
 import json
-
+import pika
 from common_utils.utils import receive_string_message, recv_n_bytes
 
 
@@ -11,19 +11,26 @@ class Loader:
                  port: int,
                  backlog: int,
                  rabbit_hostname: str,
-                 weather_queue: str,
-                 stations_queue: str,
-                 trips_queue: str,
-                 static_data_ack_queue: str):
-        signal.signal(signal.SIGTERM, lambda _n, _f: self.__close())
+                 data_exchange: str,
+                 exchange_type: str):
         self._closed = False
         try:
             self._socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
             self._socket.bind(('', port))
             self._socket.listen(backlog)
+            self._rabbit_connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=rabbit_hostname)
+            )
+            self._chan = self._rabbit_connection.channel()
+            self._data_exchange = data_exchange
+            self._chan.exchange_declare(exchange=self._data_exchange, exchange_type=exchange_type, durable=True)
         except socket.error as se:
             logging.error(f'action: socket-create | status: failed | reason: {se}')
             raise se
+        except BaseException as e:
+            logging.error(f'action: loader-init | status: failed | reason: {e}')
+            raise e
+        signal.signal(signal.SIGTERM, lambda _n, _f: self.__close())
 
     def run(self):
         logging.info('action: run | status: in progress')
@@ -38,6 +45,11 @@ class Loader:
             json_message = json.loads(message)
             if isinstance(json_message['payload'], str):
                 logging.debug(f'action: receive-client-message | status: success | message: {json_message}')
+            self._chan.basic_publish(
+                exchange=self._data_exchange,
+                routing_key=json_message['type'],
+                body=message,
+            )
 
         self.__close()
 
@@ -47,6 +59,7 @@ class Loader:
     def __close(self):
         if self._closed:
             return
+        self._rabbit_connection.close()
         self._socket.shutdown(socket.SHUT_RDWR)
         self._socket.close()
         self.closed = True
